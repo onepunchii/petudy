7267a56158f68d662c9721cf86f74d8dec37431f
최정환 <choejeonghwan@choejeonghwan-ui-MacBookPro.local>
2026-04-04 17:13:18 +0900

    fix: remove duplicate route directories (community, consult, life, partner, register, regulatory-notice, settings)

--- src/app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import RegistrationWizard from "@/components/register/RegistrationWizard";
-
-export default function RegisterPage() {
-    return (
-        <div className="flex flex-col min-h-screen bg-bg-main">
-            <RegistrationWizard />
-        </div>
-    );
-}
