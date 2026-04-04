b3a917d15b44420b472b768e4eddffb79ef8fb53
최정환 <choejeonghwan@choejeonghwan-ui-MacBookPro.local>
2026-04-04 17:12:09 +0900

    fix: remove duplicate route groups (auth) and (main)

--- src/app/(main)/page.tsx
+++
@@ -1,7 +0,0 @@
-import HomePageClient from "@/components/home/HomePageClient";
-import { getLatestPoll } from "@/actions/poll";
-
-export default async function HomePage() {
-    const poll = await getLatestPoll();
-    return <HomePageClient initialPoll={poll} />;
-}
